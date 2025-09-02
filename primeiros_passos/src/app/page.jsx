945571a278e39ef160d810e9d9c1ad06a8eb4372
FelipeMaD <felipemarinhodr@hotmail.com>
2025-09-02 14:59:09 -0300

    criando o projeto e ajustando pastas

---
+++ primeiros_passos/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Olá, mundo! tentando aprender next agora aqui nessa bomba</h1>
+    </div>
+  );
+}
