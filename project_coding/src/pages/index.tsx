c847a090b1a7bfc9c03c5568a9902ae36826b26a
FelipeMaD <felipemarinhodr@hotmail.com>
2025-09-09 15:02:12 -0300

    .

--- project_coding/src/pages/index.tsx
+++ project_coding/src/pages/index.tsx
@@ -19,6 +19,21 @@ export default function Home() {
          : (
           <p>Mierda</p>
         )}
+
+        <div>
+          <h1>Titulo</h1>
+          <div>
+            <div className='flex justify-between items-center bg'>
+              <p>Texto 12</p>
+              <p>Texto 2</p>
+            </div>
+            
+            <p>Descricao - Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Itaque provident minima suscipit voluptas ipsa laudantium sed, sequi ratione consequatur, 
+              qui ullam blanditiis tenetur iure incidunt officia? Numquam aliquid assumenda culpa?
+            </p>
+          </div>
+        </div>
     </div>
   );
 }
